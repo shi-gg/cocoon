840f271cde8d8ece20e890824b8683d2c772d920
Luna Seemann <luna@wamellow.com>
2026-02-07 18:41:13 +0100

    fix: return error for empty Did in getters (#1)

--- server/common.go
+++ server/common.go
@@ -4,12 +4,16 @@ import (
 	"context"
 
 	"github.com/haileyok/cocoon/models"
+	"gorm.io/gorm"
 )
 
 func (s *Server) getActorByHandle(ctx context.Context, handle string) (*models.Actor, error) {
 	var actor models.Actor
 	if err := s.db.First(ctx, &actor, models.Actor{Handle: handle}).Error; err != nil {
 		return nil, err
+	}
+	if actor.Did == "" {
+		return nil, gorm.ErrRecordNotFound
 	}
 	return &actor, nil
 }
@@ -19,6 +23,9 @@ func (s *Server) getRepoByEmail(ctx context.Context, email string) (*models.Repo
 	if err := s.db.First(ctx, &repo, models.Repo{Email: email}).Error; err != nil {
 		return nil, err
 	}
+	if repo.Did == "" {
+		return nil, gorm.ErrRecordNotFound
+	}
 	return &repo, nil
 }
 
@@ -27,6 +34,9 @@ func (s *Server) getRepoActorByEmail(ctx context.Context, email string) (*models
 	if err := s.db.Raw(ctx, "SELECT r.*, a.* FROM repos r LEFT JOIN actors a ON r.did = a.did WHERE r.email= ?", nil, email).Scan(&repo).Error; err != nil {
 		return nil, err
 	}
+	if repo.Repo.Did == "" {
+		return nil, gorm.ErrRecordNotFound
+	}
 	return &repo, nil
 }
 
@@ -34,6 +44,9 @@ func (s *Server) getRepoActorByDid(ctx context.Context, did string) (*models.Rep
 	var repo models.RepoActor
 	if err := s.db.Raw(ctx, "SELECT r.*, a.* FROM repos r LEFT JOIN actors a ON r.did = a.did WHERE r.did = ?", nil, did).Scan(&repo).Error; err != nil {
 		return nil, err
+	}
+	if repo.Repo.Did == "" {
+		return nil, gorm.ErrRecordNotFound
 	}
 	return &repo, nil
 }
